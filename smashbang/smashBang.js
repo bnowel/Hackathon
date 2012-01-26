11fc495abf39bba8f3da026725cc51de017c508b
matt-holden <jftholden@yahoo.com>
2012-01-26 03:07:46 +0000

    matt smashes it and bangs it

--- smashbang/smashBang.js
+++ smashbang/smashBang.js
@@ -1,3 +1,4 @@
+
 (function(){
     
 	var randomSort = function(a,b) {
@@ -37,16 +38,15 @@
 	var ElementData = function(el){
 
 		var $el = $(el);
-		this.id = $el.attr('id');
-		this.classes = ($el.attr("class") || "").split(" ");
 		this.oldCSS = {
-			position: el.style.position,
+    		position: el.style.position,
 			top: el.style.top,
 			left: el.style.left,
-			opactiy: el.style.opacity,
 			backgroundColor: el.style.backgroundColor,
 			color: el.style.color
 		};
+        this.id = $el.attr('id');
+		this.classes = $el.attr("class")
 	}
 	
 	function smashIt(element) {
@@ -55,27 +55,25 @@
 		var winWidth = $(window).width();
 		var winHeight = $(window).height();
 		
-		var myOriginalData = new ElementData(element);
-		$this.data("originalData", myOriginalData);
-		
+        var myOriginalData = $this.data("originalData");
+        if (!myOriginalData){
+    		myOriginalData = new ElementData(element);
+            $this.data("originalData", myOriginalData);
+        };
+        
+        
 		var newCSS = {
-		position:'absolute',
+    		position:'absolute',
 			top: Math.max(winHeight - randomOffset(winHeight) - $(this).height(), 0) + 'px',
-			left: Math.max(winWidth - randomOffset(winWidth) - $(this).width(), 0) + 'px',
-			opacity: (Math.random() + 0.5).toString()
+			left: Math.max(winWidth - randomOffset(winWidth) - $(this).width(), 0) + 'px'
 		};
+        
 		if (oneInThree())
 			$.extend(newCSS, {'background-color': getRandomColor() });
 		if (oneInThree())
 			$.extend(newCSS, {'color': getRandomColor() });
 		if (oneInThree())
 			$this.removeAttr("id");
-
-		$this.removeClass(myOriginalData.classes 
-			&& myOriginalData.classes.length 
-			&& myOriginalData.classes[randomTo(myOriginalData.classes.length)]
-		);
-		
 		
 		$this.css(newCSS);
 	};
@@ -85,11 +83,14 @@
 		var $el = $(element);
 		
 		var myOriginalData = $el.data('originalData');
-		
-		$el
-		.addClass(myOriginalData.classes)
-		.css( myOriginalData.oldCss )
-		.attr('id',myOriginalData.id);
+        
+        if (!myOriginalData){
+            console.log("SUCK IT");
+            return;
+        }
+	//	$el.addClass(myOriginalData.classes);
+		$el.css( myOriginalData.oldCSS );
+		$el.attr('id',myOriginalData.id);
 	};
 
 	//INITIALIZE THE SMASHBANG
@@ -97,25 +98,43 @@
 		//get all nodes;
 		if (!window.jQuery){
 			/*! jQuery v1.7.1 jquery.com | jquery.org/license */
-			console.log("this page needs jQuery");
 			return;
 		}
 				
 		var $allElements = $("*").not('html, body, script, style');
+        if ($allElements.length > 250) $allElements = $allElements.slice(0,100);
 		$allElements = Array.prototype.sort.call($allElements, randomSort);
-
+        var fns2Execute = [];
 		for ( var x=0;  x < 4; x++ ){
 			for ( var q=0; q < $allElements.length; q++ ) {
-				x==0 && smashIt($allElements[q]);
-				x==1 && (!oneInFour ? smashIt($allElements[q]) : bangIt($allElements[q]));
-				x==2 && (oneInThree ? smashIt($allElements[q]) : bangIt($allElements[q]));
-				x==3 && bangIt($allElements[q]);
+                fns2Execute.push((function(x,q){
+                    return function(){
+        				x==0 && smashIt($allElements[q]);
+        				x==1 && (!oneInFour() ? smashIt($allElements[q]) : bangIt($allElements[q]));
+        				x==2 && (oneInThree() ? smashIt($allElements[q]) : bangIt($allElements[q]));
+        				x==3 && bangIt($allElements[q]);
+                    };
+                })(x,q));
 			}
-		}			
-
+		}
+        
+        var y = 0;
+        function exec() {
+             
+            if (y == fns2Execute.length) return;
+            fns2Execute[y]();
+            y++;            
+            setTimeout(function(){
+                exec();
+            }, 5);
+        };
+        exec();
+        
+        window.fns2Execute = fns2Execute;
     };
     
 })();
 
-$.smashBang();
-
+$(function(){
+    $.smashBang();
+});
