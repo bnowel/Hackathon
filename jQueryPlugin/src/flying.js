8c0da6a821542177dd2e051f9c1e0150611f4fcd
algard <david.algar@mindbodyonline.com>
2012-01-27 19:09:51 +0000

    changing movements to right-left

--- jQueryPlugin/src/flying.js
+++ jQueryPlugin/src/flying.js
@@ -1,10 +1,16 @@
+// -1 = left, 0 no movement, +1 = right
+var flyingDirection = 0;
+
+$("#left_scroll").hover( function() { flyingDirection= -1 ; }, function () { flyingDirection= 0 ;});
+$("#right_scroll").hover( function() { flyingDirection= 1 ; }, function () { flyingDirection= 0 ;});
+
 (function( $ ){ 
     
      $.fn.startFlying = function(  ) { 
          var childrens = $(this).children(".listitem");
          var top = 10;
          childrens.each( function ( ) {      
-                $(this).css("top",top+"px");
+                $(this).css("left",top+"px");
                 top += 30;
              });
              
@@ -15,12 +21,14 @@
             }
             console.log("about to delay");
              })
-             ,1000 );
+             ,500 );
         };
     
      
      function checkForNewElementAndRetrieve(){         
-         $(".listitem:eq(0)").before($("<div class='listitem'> More trees! </div>"));
+         if(flyingDirection != 0){
+            $(".listitem:eq(0)").before($("<div class='listitem'> More trees! </div>"));
+         }
          return true;
      }
      
@@ -28,18 +36,19 @@
         var childrens = $("#list_container").children(".listitem");
         
         childrens.each( function ( ) {      
-                var curTop = $(this).css("top");
+                var curTop = $(this).css("left");
                 
                 
                 curTop = curTop.replace("px", "");
-                var newTop = parseInt(curTop) +10;
+                
+                var newTop = parseInt(curTop) + flyingDirection;
                 
                 //TODO this doesn't work at the moment, so things keep moving down off the page, lame
                 if(newTop >= 500){
                     $(this).remove();
                 }
                 
-                $(this).css("top",newTop+"px");
+                $(this).css("left",newTop+"px");
         });
      }
 
