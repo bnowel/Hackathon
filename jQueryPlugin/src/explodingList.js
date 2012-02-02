389b5a5eb84f2e2c36523f27eeac35dc11760c53
algard <david.algar@mindbodyonline.com>
2012-02-02 02:07:05 +0000

    adding explodingList, and creating css directory for styles

--- jQueryPlugin/src/explodingList.js
+++ jQueryPlugin/src/explodingList.js
@@ -1,4 +1,20 @@
-onload=function(){
+(function( $ ){
+    
+    $.fn.toggleAble = function(  ) { 
+        $(this).toggleClass("selected");
+        console.log("toggled"); 
+
+    };
+      
+    $.fn.showHideChildren = function ( ) {
+        $(this).live("click", function(){
+                            showOrHideChildren($(this), "apiMethodList");
+                            }
+            );
+    };
+
+
+function hi(){
     $(".apiServiceHeader").live("click", function(){
             showOrHideChildren($(this), "apiMethodList");
         });
@@ -12,8 +28,15 @@ onload=function(){
         console.log("toggled");
     });
 };
-
+/*  disabled 
 function showOrHideChildren(id, targetClass){
     console.log("Showing this: " + id);
     id.parent().children("." + targetClass).toggleClass('hidden');
+}*/
+
+function showOrHideChildren(id){
+    console.log("Hideshowing "+id);
+    id.parent().children().toggleCLass('hidden');
 }
+
+})( jQuery );
